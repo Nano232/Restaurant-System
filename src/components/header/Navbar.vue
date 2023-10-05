4445dde3b291bd9f9a56284c28a02e612c5b965a
mohamed <elzorany.mohamed232@gmail.com>
2023-10-05 21:26:37 +0300

    update

---
+++ src/components/header/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul id="nav">
+    <router-link :to="{ name: 'home' }"> Home </router-link>
+    <a href="#" @click="logMeOut()">Logout</a>
+  </ul>
+</template>
+<script>
+export default {
+  name: "NavBar",
+  methods: {
+    logMeOut() {
+      localStorage.clear();
+      this.$router.push({ name: "LogInPage" });
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+$fontColor: darkcyan;
+$navColor: #525a5a;
+ul {
+  display: flex;
+  justify-content: center;
+  list-style: none;
+  margin-bottom: 50px;
+  a {
+    text-decoration: none;
+    font-weight: bold;
+    color: $fontColor;
+    padding: 10px;
+  }
+}
+a.router-link-exact-active a,
+ul a a:hover {
+  color: $navColor;
+  box-sizing: 2px 2px 4px 0px $navColor;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,15 @@
 <template>
+  <Navbar />
   <div class="home">Home</div>
 </template>
 
 <script>
+import Navbar from "@/components/header/Navbar.vue";
 export default {
   name: "HomePage",
+  components: {
+    Navbar,
+  },
 
   mounted() {
     let user = localStorage.getItem("user-info");
