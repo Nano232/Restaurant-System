78978221dab6773559568bc1c3240860e4d0d252
mohamed <elzorany.mohamed232@gmail.com>
2023-10-06 11:26:23 +0300

    update

--- src/components/profile/UpdateProfile.vue
+++ src/components/profile/UpdateProfile.vue
@@ -13,6 +13,9 @@
               placeholder="Enter Your Name"
               v-model="name"
             />
+            <span class="error-feedback" v-if="v$.name.$error">{{
+              v$.name.$errors[0].$message
+            }}</span>
           </div>
           <div class="col-12 m-3">
             <input
@@ -21,6 +24,9 @@
               placeholder="Enter Your Email"
               v-model="email"
             />
+            <span class="error-feedback" v-if="v$.email.$error">{{
+              v$.email.$errors[0].$message
+            }}</span>
           </div>
           <div class="col-12 m-3">
             <input
@@ -29,11 +35,19 @@
               placeholder="Enter Your Password"
               v-model="pass"
             />
+            <span class="error-feedback" v-if="v$.pass.$error">{{
+              v$.pass.$errors[0].$message
+            }}</span>
           </div>
           <div class="col-12 m-3">
-            <button type="button" class="btn btn-primary">
+            <button type="button" class="btn btn-primary" @click="upDateNow()">
               Update Profile Now
             </button>
+          </div>
+        </div>
+        <div class="row align-items-center text-center" style="margin: 0 auto">
+          <div class="col-12">
+            <span>{{ updateErr }}</span>
           </div>
         </div>
       </form>
@@ -42,6 +56,10 @@
 </template>
 <script>
 import Navbar from "@/components/header/Navbar.vue";
+import { useVuelidate } from "@vuelidate/core";
+import { required, email, minLength } from "@vuelidate/validators";
+const sweetalert = require("sweetalert");
+import axios from "axios";
 export default {
   name: "UpdateProfile",
   components: {
@@ -49,9 +67,19 @@ export default {
   },
   data() {
     return {
+      v$: useVuelidate(),
       name: "",
-      email: "",
       pass: "",
+      email: "",
+      userId: "",
+      updateErr: "",
+    };
+  },
+  validations() {
+    return {
+      name: { required, minLength: minLength(3) },
+      pass: { required, minLength: minLength(8) },
+      email: { required, email },
     };
   },
   mounted() {
@@ -60,7 +88,46 @@ export default {
       this.name = JSON.parse(user).name;
       this.email = JSON.parse(user).email;
       this.pass = JSON.parse(user).pass;
+      this.userId = JSON.parse(user).id;
     }
+  },
+  methods: {
+    async upDateNow() {
+      this.v$.$validate();
+      if (!this.v$.$error) {
+        console.log("Valiated");
+        let result = await axios.put(
+          `http://localhost:3000/users/${this.userId}`,
+          {
+            name: this.name,
+            email: this.email,
+            pass: this.pass,
+          }
+        );
+        console.log(result);
+        if (result.status == 200) {
+          sweetalert({
+            title: "Profile Updated Successfully",
+            icon: "success",
+          });
+          localStorage.setItem("user-info", JSON.stringify(result.data));
+          this.$router.push({ name: "ProfilePage" });
+        } else {
+          sweetalert({
+            title: "Profile Updated Fild",
+            icon: "error",
+          });
+        }
+      } else {
+        this.updateErr = "Something Went Wrong, Refresh The Page";
+      }
+    },
   },
 };
 </script>
+<style lang="scss" scoped>
+span {
+  color: red;
+  font-size: 0.85em;
+}
+</style>
